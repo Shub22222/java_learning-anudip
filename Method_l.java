33afcc596c545b4d1ade155e7c178d6fee05f07b
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-07-07 22:41:48 +0530

    now i understand how make method!!

---
+++ Method_l.java
@@ -0,0 +1,9 @@
+public class Method_l {
+    static void mymethod(){
+        System.out.println("hello world");
+
+    }
+    public static void main (String args[]){
+        mymethod();
+    }
+}

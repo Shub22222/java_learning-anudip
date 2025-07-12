38b0931dec298215ba9c42a07214d1fdf73f2350
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-07-12 15:17:16 +0530

    Create Addition method.java

---
+++ Addition method.java
@@ -0,0 +1,11 @@
+public class vada{
+    static void add(int a,int b){
+        int num=a+b;
+        System.out.println(num);
+    }
+    
+    public static void main(String args[]){
+        add(2,9);
+    }
+    
+}

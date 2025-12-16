e49c95bb0c372f89f87f48cda5565f3739249e18
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-12-16 20:04:25 +0530

    Update print_one_to_5.java

--- print_one_to_5.java
+++ print_one_to_5.java
@@ -2,3 +2,9 @@ public class print_one_to_5 {
     public static void main(String[] args) {
         for(int i=1; i<=5;i++){
             for(int j=1;j<=i;j++){
+              System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}

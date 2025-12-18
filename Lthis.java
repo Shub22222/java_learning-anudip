eabed179d7dcf19f35d8f8f33a1296d151d5273d
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-12-18 21:56:17 +0530

    Create Lthis.java

---
+++ Lthis.java
@@ -0,0 +1,12 @@
+public class Lthis {
+    int x;
+
+    public Lthis(int x){
+        this.x=x;
+    }
+    public static void main(String[] args) {
+        Lthis myobj=new Lthis(5);
+        System.out.println(myobj.x);
+    }
+  
+}

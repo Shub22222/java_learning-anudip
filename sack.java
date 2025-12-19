310e5cc0506f2072e4537994be3b74a0fadcef88
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-12-19 16:18:39 +0530

    Create sack.java

---
+++ sack.java
@@ -0,0 +1,8 @@
+public class sack {
+    private String news="hi, you have whole world as gift enjoy buddy!!!!!!!!";
+
+
+    public String open_sack(){    // getter method
+        return news;
+    }
+}

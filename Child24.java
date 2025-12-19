42fc84d4457307bec1480a00522f7c10c60cf3b5
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-12-19 21:48:50 +0530

    Create Child24.java

---
+++ Child24.java
@@ -0,0 +1,14 @@
+public class Child24 extends Parent1 {
+    
+    
+    public static void main(String[] args) {
+        Child24 obj=new Child24();
+        // obj.show();
+        message();
+        System.out.println(obj.name_car);
+
+        }
+
+
+
+}

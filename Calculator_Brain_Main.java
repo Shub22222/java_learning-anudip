92485068dd6de83af3bd689c32d0ea1aba7ab8e0
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-12-20 19:14:39 +0530

    Create Calculator_Brain_Main.java

---
+++ Calculator_Brain_Main.java
@@ -0,0 +1,9 @@
+public class Calculator_Brain_Main extends Calculator_Brain{
+    public static void main(String[] args) {
+        Calculator_Brain_Main obj=new Calculator_Brain_Main();
+        System.out.println(obj.add(1, 1));
+        System.out.println(obj.sub(1, 1));
+        System.out.println(obj.mul(1, 1));
+        System.out.println(obj.div(1, 1));
+    }
+}

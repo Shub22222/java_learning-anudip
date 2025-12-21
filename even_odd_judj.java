895498286ab9efda7cdc450a25b7f8e05c4669cf
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-12-21 16:08:05 +0530

    Create even_odd_judj.java
    // ### Task 2️⃣: Even or Odd Judge
    
    // * Create a method `checkEvenOdd(int num)`
    // * Method should **print** whether number is even or odd
    // * Call it 3 times with different values

---
+++ even_odd_judj.java
@@ -0,0 +1,16 @@
+public class even_odd_judj {
+    int checkEvenOdd(int num){
+        if (num%2==0){
+
+            System.out.println("it's even "+ num);
+            
+        }
+        else{
+            System.out.println("it's odd "+ num);
+
+        }
+        return num;
+
+    }
+    
+}

7b4ebb0a02080af54bf71544a284a6e121141abc
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-12-21 16:08:43 +0530

    Create lets_calculateit.java
    // ### Task 2️⃣: Even or Odd Judge
    
    // * Create a method `checkEvenOdd(int num)`
    // * Method should **print** whether number is even or odd
    // * Call it 3 times with different values
    
    // ---

---
+++ lets_calculateit.java
@@ -0,0 +1,8 @@
+public class lets_calculateit extends even_odd_judj {
+    public static void main(String[] args) {
+        lets_calculateit obj=new lets_calculateit();
+        obj.checkEvenOdd(2);
+        obj.checkEvenOdd(456);
+        obj.checkEvenOdd(265);
+    }
+}

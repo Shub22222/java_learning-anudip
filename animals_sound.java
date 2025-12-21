97528dc5abfcc8c38c0750a6ecb6a7d724dfbaa5
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-12-21 20:09:49 +0530

    Create animals_sound.java

---
+++ animals_sound.java
@@ -0,0 +1,13 @@
+class animals_sound  {
+
+    public static void main(String[] args) {
+        animal obj=new animal();
+        pig obj1=new pig();
+        animal obj3=new Dog();
+        obj.animalsound();
+        obj1.animalsound();
+        obj3.animalsound();
+    }
+
+    
+}

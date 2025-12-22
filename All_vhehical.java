e6c946365a921b1cfe634d690a160b8db0ac9949
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-12-22 20:33:16 +0530

    Create All_vhehical.java

---
+++ All_vhehical.java
@@ -0,0 +1,18 @@
+package abstrct;
+
+public class All_vhehical {
+    
+    public static void main(String[] args) {
+        Vheical obj=new Bike();
+        Vheical obj1=new car();
+        obj.start();
+        obj1.start();
+        
+
+
+        
+
+            
+        
+    }
+}

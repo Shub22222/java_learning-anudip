401082f008af23faac7aedde97ee881b509adf38
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-12-24 20:19:40 +0530

    Create Odd_even.java

---
+++ Odd_even.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Odd_even {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();   // program STOPS here ⏸️
+
+        if (num % 2 == 0) {
+            System.out.println("The number is even " + num);
+        } else {
+            System.out.println("The number is odd " + num);
+        }
+    }
+}

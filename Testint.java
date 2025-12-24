945d3ea916f48b680ec2a70c18f6ff497eab4d35
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-12-24 20:02:26 +0530

    Create Testint.java
    just string to int

---
+++ Testint.java
@@ -0,0 +1,15 @@
+public class Testint {
+    public static void main(String[] args) {
+        
+        String a="10";
+        int n=Integer.parseInt(a);
+
+
+      
+
+        System.out.println(n);
+
+        
+     
+    }
+}

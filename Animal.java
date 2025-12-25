3eb3ac9c2ebc1009e65ed7acacf88e999181c4f8
Shub22222 <130507915+Shub22222@users.noreply.github.com>
2025-12-25 20:13:47 +0530

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,10 @@
+
+
+ class Dog implements Animal {
+    public void sound(){
+        System.out.println("Dog barks");
+    }
+    public void sleep(){
+        System.out.println("Dog sleep");
+    }
+}

6640647d165bdc2055a8fb6dd3bbcd97b9fd8fed
Shubham <130507915+Shub22222@users.noreply.github.com>
2025-12-27 20:22:28 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,10 @@
+public class string {
+     
+        public static void main(String args[]){
+            // System.out.printf("hello%10s");
+            System.out.printf("hello%10s", "Shivam");
+
+            System.out.println("world ");
+
+        }
+}

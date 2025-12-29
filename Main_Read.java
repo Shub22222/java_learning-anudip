4bdcd336f06b86542429d2b85d19a39026c30f61
Shubham <130507915+Shub22222@users.noreply.github.com>
2025-12-29 16:21:27 +0530

    Create Main_Read.java

---
+++ Main_Read.java
@@ -0,0 +1,18 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+public class Main_Read {
+    public static void main(String[] args) {
+        try(FileInputStream obj=new FileInputStream("Data.txt")){
+            int i;
+
+            while ((i=obj.read())!=-1) {
+                System.out.print((char)i);
+                
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+
+        }
+    }
+}

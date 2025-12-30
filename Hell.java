33407bc7899b32e76f1df5444cb48afa81f84c76
Shubham <130507915+Shub22222@users.noreply.github.com>
2025-12-30 23:27:07 +0530

    Create Hell.java

---
+++ Hell.java
@@ -0,0 +1,18 @@
+public interface Hell {
+    public static void main(String [] args){
+        /*System.out.println("Hello world");
+        System.out.println("Hello World!");
+System.out.println("Have a good day!");
+*/
+//  System.out.println(3+3);
+
+
+        // String name = "bob";
+        // final int num=2;
+        // num=54;
+        // float num=2.645f;
+        // char num='d';
+        boolean num=true;
+        System.out.println(num);
+    }
+}

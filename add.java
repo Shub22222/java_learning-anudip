54662a23b88ff1fc54238ea35f378fb22d169224
Shubham <130507915+Shub22222@users.noreply.github.com>
2025-12-30 23:28:14 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,11 @@
+
+public class add {
+    public static void main(String args[]){
+    int a=5;
+    int b=5;
+    int sum =a+b;
+    int sub =a-b;
+    System.out.println(sum);
+    System.out.print(sub);
+}
+}

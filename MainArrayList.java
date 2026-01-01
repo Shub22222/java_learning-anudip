79cb80d81c4be0f891a9a1d5cb5d601d74be2203
Shubham <130507915+Shub22222@users.noreply.github.com>
2026-01-01 15:07:46 +0530

    Create MainArrayList to demonstrate ArrayList usage

---
+++ MainArrayList.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class MainArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> cars=new ArrayList<String>();
+        cars.add("volvo");
+        cars.add("Ford");
+        cars.add("BMW");
+        System.out.println(cars);
+    }
+}

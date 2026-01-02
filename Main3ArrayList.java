25080bd9f44da0ab77cb624092f7e1dbb23be89c
Shubham <130507915+Shub22222@users.noreply.github.com>
2026-01-02 15:11:15 +0530

    Implement Main3ArrayList with boat examples

---
+++ Main3ArrayList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Main3ArrayList{
+    public static void main(String[] args) {
+        ArrayList<String> boats=new ArrayList<String>();
+        boats.add("Canoe");
+        boats.add("Yacht");
+        boats.add("farry");
+        boats.add("Speedboat");
+        boats.add("Barge");
+        
+        boats.add(0,"Kayak");
+        System.out.println("all boats list "+boats);  
+        System.out.println(boats.get(3));  
+      }
+}
+
+

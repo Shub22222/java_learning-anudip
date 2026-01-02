9a8d824f704a3641609b8fd35dd91048fddb0558
Shubham <130507915+Shub22222@users.noreply.github.com>
2026-01-02 15:05:38 +0530

    Create Main1ArrayList.java

---
+++ Main1ArrayList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main1ArrayList{
+    public static void main(String[] args) {
+        ArrayList<String> boats=new ArrayList<String>();
+        boats.add("Canoe");
+        boats.add("Yacht");
+        boats.add("farry");
+        boats.add("Speedboat");
+        boats.add("Barge");
+
+        System.out.println("all boats list "+boats);  
+      }
+}
+
+

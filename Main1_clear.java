e04540ec33fbce8e37631fe9768653c89e51e984
Shubham <130507915+Shub22222@users.noreply.github.com>
2026-01-02 16:39:50 +0530

    Create Main1_clear.java

---
+++ Main1_clear.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Main1_clear{
+    public static void main(String[] args) {
+        ArrayList<String> boats=new ArrayList<String>();
+        boats.add("Canoe");
+        boats.add("Yacht");
+        boats.add("ferry");
+        boats.add("Speedboat");
+        boats.add("Barge");
+        
+        boats.add(0,"Kayak");
+        System.out.println("all boats list "+boats); 
+        boats.set(2, "Catamaran") ;
+        System.out.println(boats.get(3));  
+        System.out.println(boats);  
+        boats.remove("Barge");
+        System.out.println(boats);  
+        boats.clear();
+        System.out.println(boats);  
+
+      }
+}
+
+

5631221291ee8f3a9a326b755b84395f3a84a68d
Shubham <130507915+Shub22222@users.noreply.github.com>
2026-01-03 19:35:13 +0530

    Add Practice_linkdlinst.java with LinkedList example

---
+++ Practice_linkdlinst.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class Practice_linkdlinst {
+    public static void main(String[] args) {
+        LinkedList<String> Planets = new LinkedList<String>();
+        Planets.add("Eearth");
+        Planets.add("Sun");
+
+        System.out.println(Planets);
+    }    
+}

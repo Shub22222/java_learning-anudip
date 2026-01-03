cf131d91df8e776b94a19e07e723e936d7ac0713
Shubham <130507915+Shub22222@users.noreply.github.com>
2026-01-03 19:26:31 +0530

    Create var_Arraylist.java to demonstrate ArrayList usage

---
+++ var_Arraylist.java
@@ -0,0 +1,13 @@
+
+
+import java.util.ArrayList;
+public class var_Arraylist {
+    public static void main(String[] args) {
+        var cars=new ArrayList<String>();
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Volvo");
+
+        System.out.print(cars);
+    }
+}

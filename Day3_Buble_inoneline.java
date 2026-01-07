59c320b06f80528a33e0cd333af97529622ae941
Shubham <130507915+Shub22222@users.noreply.github.com>
2026-01-07 21:38:33 +0530

    Add bubble sort implementation in Day3_Buble_inoneline.java
    Implemented bubble sort algorithm to sort an array.

---
+++ Day3_Buble_inoneline.java
@@ -0,0 +1,21 @@
+public class Day3_Buble_inoneline {
+
+     public static void main(String[] args) {
+        //array crea,ed
+        int [] arr={80,20,10,5}; 
+        // this outer loop runs throgh all elements
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+
+                }
+            }
+        }
+        for(int i:arr){
+            System.out.print(i +" ");
+        }
+     }
+}

fd9f4be817205171d10bd2df379fb642265bc81b
Shubham <130507915+Shub22222@users.noreply.github.com>
2026-01-08 14:56:46 +0530

    Create take_give.java

---
+++ take_give.java
@@ -0,0 +1,11 @@
+
+
+public class take_give {
+    public static void main(String[] args) {
+        int b=5;
+        for(int i=1;i<=b;i++){
+            System.out.println(i);
+        }
+    }
+
+}

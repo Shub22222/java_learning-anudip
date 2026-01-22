71c61d60de2f8a800a6678d461c59bae9338433a
Shubham <130507915+Shub22222@users.noreply.github.com>
2026-01-22 19:48:00 +0530

    Create factorial_practice1.java

---
+++ factorial_practice1.java
@@ -0,0 +1,14 @@
+public class factorial_practice1 {
+    public static void main(String[] args) {
+        
+        int a=5;
+        int fact=1;
+        
+        while(a>0){
+            fact=fact*a;
+            a--;
+            
+        }
+        System.out.println(fact);
+    }
+}
